fc9377be2cd7661b6c3b5891592aeba5bb8e6ca0
francisco <ortegareal1999@gmail.com>
2024-02-14 19:59:03 -0300

    Terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import CharacterList from './components/CharacterList'
 
 function App() {
-
-  return <div>
-      <h1>Rick and Morty</h1>
+  return <div className='bg-dark text-white'>
+      <h1 className='text-center display-1 py-4'>Rick and Morty</h1>
       <CharacterList/>
     </div>
 }
